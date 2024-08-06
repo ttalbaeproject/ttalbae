b377619c1557a01a95172d6de335dc1bd5b59a6f
minyee2913 <leegm0759@gmail.com>
2024-08-06 14:57:41 +0900

    player movement & sprites

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,84 @@
+
+
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public BoxCollider2D col;
+    public Player pl;
+    public bool isRunning, isJumping, isMoving, canMove;
+
+    [HideInInspector] public Vector3 pos { get { return transform.position; } }
+
+    void Start()
+    {
+        pl = Player.Main;
+
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        pl = GetComponent<Player>();
+    }
+
+    public void Push(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+        isJumping = true;
+        pl.animator.SetTrigger("jump");
+    }
+
+    public void ActivateRb()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void DesactivateRb()
+    {
+        // rb.velocity = Vector3.zero;
+        // rb.angularVelocity = 0f;
+        // rb.isKinematic = true;
+    }
+
+    public float speed = 5f;
+    void FixedUpdate()
+    {
+        isMoving = false;
+        
+        if (canMove) {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+                isMoving = true;
+
+                pl.facingRight = false;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                isMoving = true;
+
+                pl.facingRight = true;
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);
+                isMoving = true;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, 0, -speed * Time.deltaTime);
+                isMoving = true;
+            }
+        }
+
+        if (pl.OnGround() && rb.velocity.y <= 0) {
+            rb.velocity = Vector2.zero;
+            isJumping = false;
+        }
+
+        pl.animator.SetBool("isMoving", isMoving);
+        pl.animator.SetBool("isJumping", isJumping);
+        pl.animator.SetBool("isRunning", isRunning);
+    }
+}
---
+++ Assets/Scripts/System/DragSystem.cs
@@ -0,0 +1,129 @@
+
+
+using UnityEngine;
+
+public class DragSystem : MonoBehaviour
+{
+    #region Singleton class: GameManager
+
+    public static DragSystem Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    #endregion
+
+    Camera cam;
+
+    public Movement ball;
+    public Trajectory trajectory;
+    [SerializeField] float pushForce = 4f;
+    [SerializeField] float maxDistance;
+
+    bool isDragging = false;
+    bool bySpace;
+
+    Vector2 startPoint;
+    Vector2 endPoint;
+    Vector2 direction;
+    Vector2 force;
+    float distance;
+
+    //---------------------------------------
+    void Start()
+    {
+        cam = Camera.main;
+        ball.DesactivateRb();
+    }
+
+    void Update()
+    {
+        if (ball.rb.velocity.y <= 0) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                isDragging = true;
+                bySpace = true;
+                OnDragStart();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                bySpace = false;
+                OnDragStart();
+            }
+        }
+
+        if (isDragging)
+        {
+            OnDrag();
+
+            if (bySpace) {
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    isDragging = false;
+                    OnDragEnd();
+                }
+            } else {
+                if (Input.GetMouseButtonUp(0))
+                {
+                    isDragging = false;
+                    OnDragEnd();
+                }
+            }
+        }
+    }
+
+    //-Drag--------------------------------------
+    void OnDragStart()
+    {
+        startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        trajectory.Show();
+        ball.canMove = false;
+        ball.pl.animator.SetBool("isReady", true);
+    }
+
+    void OnDrag()
+    {
+        endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+        distance = Vector2.Distance(startPoint, endPoint);
+
+        if (endPoint.x < startPoint.x) {
+            ball.pl.facingRight = true;
+        } else {
+            ball.pl.facingRight = false;
+        }
+
+        if (distance > maxDistance) {
+            distance = maxDistance;
+        }
+
+        direction = (startPoint - endPoint).normalized;
+        force = direction * distance * pushForce;
+
+        //just for debug
+        Debug.DrawLine(startPoint, endPoint);
+
+
+        trajectory.UpdateDots(ball.pos + new Vector3(0, -1), force);
+    }
+
+    void OnDragEnd()
+    {
+        //push the ball
+        ball.ActivateRb();
+
+        ball.Push(force);
+
+        ball.pl.animator.SetBool("isReady", false);
+
+        ball.canMove = true;
+
+        trajectory.Hide();
+    }
+
+}
