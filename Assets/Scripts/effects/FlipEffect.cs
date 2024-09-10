28282a40f913e94ed3cd6ad8d4ee0fdaa91de482
minyee2913 <leegm0759@gmail.com>
2024-09-11 00:45:27 +0900

    seperate

--- Assets/Scripts/effects/FlipEffect.cs
+++ Assets/Scripts/effects/FlipEffect.cs
@@ -8,25 +8,20 @@ public class FlipEffect : Effect
 
     public override string Name => "좌우 반전";
 
-    public float speed = -1;
-
     public FlipEffect(float dur) : base(dur)
     {
     }
 
     public override void OnEnd()
     {
-        var movement = Player.Main.GetComponent<Movement>();
+        Player.Main.isFlipped = false;
         
         Player.Main.render.color = Player.Main.spriteCol;
-        movement.speed = movement.defSpeed;
     }
 
     public override void OnStart()
     {
-        var movement = Player.Main.GetComponent<Movement>();
-
-        movement.speed = movement.defSpeed * speed;
+        Player.Main.isFlipped = true;
         Player.Main.render.color = effectColor;
         
         Player.Main.EffectIndicate(Player.Main.bad);
